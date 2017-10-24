2abe93713e18524a75eab25da37bb62599805380
cupsos <cupsos@gmail.com>
2017-10-24 22:49:15 +0900

    VisualStudio Clear가 솔루션 파일도 삭제함

--- Editor/VisualStudio.cs
+++ Editor/VisualStudio.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace MhuUtility
 {
@@ -9,15 +11,21 @@ namespace MhuUtility
         private static readonly string PROJECT_ROOT = Directory.GetParent(Application.dataPath).FullName;
         private static readonly string TEMP_BIN = Path.Combine(PROJECT_ROOT, "Temp", "UnityVS_bin");
         private static readonly string TEMP_OBJ = Path.Combine(PROJECT_ROOT, "Temp", "UnityVS_obj");
+        private static readonly string VS_CONFIG = Path.Combine(PROJECT_ROOT, ".vs");
+        private static readonly string[] TEMP_DIRS = { TEMP_BIN, TEMP_OBJ, VS_CONFIG };
+        private static readonly Regex SOLUTION_FILE = new Regex(@"\.(csproj|sln)$");
 
         [MenuItem("MhuUtility/VisualStudio/Clear")]
         private static void Clear()
         {
-            if (Directory.Exists(TEMP_BIN))
-                Directory.Delete(TEMP_BIN, true);
-
-            if (Directory.Exists(TEMP_OBJ))
-                Directory.Delete(TEMP_OBJ, true);
+            foreach (string path in TEMP_DIRS)
+                if (Directory.Exists(path))
+                    Directory.Delete(path, true);
+            
+            foreach (string solutionFile in (from file in Directory.GetFiles(PROJECT_ROOT)
+                                             where SOLUTION_FILE.IsMatch(file)
+                                             select file))
+                File.Delete(solutionFile);
         }
     }
 }
