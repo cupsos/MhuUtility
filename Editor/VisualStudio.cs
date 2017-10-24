a9d76ebbce49049aa023382451391cd2209213ee
cupsos <cupsos@gmail.com>
2017-10-24 14:34:34 +0900

    VisualStudio 임시파일 제거하는 숏컷 추가
    VisualStudio를 재시작하여도 새로 추가한 소스 파일의 IntelliSense가
    작동하지 않는 경우, 임시파일을 제거하고 재시작하는 것으로 해결 가능.
    버그로 추정됨

---
+++ Editor/VisualStudio.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+namespace MhuUtility
+{
+    public class VisualStudio : EditorWindow
+    {
+        private static readonly string PROJECT_ROOT = Directory.GetParent(Application.dataPath).ToString();
+        private static readonly string TEMP_BIN = Path.Combine(PROJECT_ROOT, "Temp", "UnityVS_bin");
+        private static readonly string TEMP_OBJ = Path.Combine(PROJECT_ROOT, "Temp", "UnityVS_obj");
+
+        [MenuItem("MhuUtility/VisualStudio/Clear")]
+        private static void Clear()
+        {
+            if (Directory.Exists(TEMP_BIN))
+                Directory.Delete(TEMP_BIN, true);
+
+            if (Directory.Exists(TEMP_OBJ))
+                Directory.Delete(TEMP_OBJ, true);
+        }
+    }
+}
