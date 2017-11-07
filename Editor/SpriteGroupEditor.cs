0b50bc2b3d7c8c548653ee8543ee04a8b67daf3d
cupsos <cupsos@gmail.com>
2017-11-08 01:14:55 +0900

    SpriteGroup 추가
    Canvas Group에서 아이디어 얻음.
    자식객체의 SpriteRenderer 알파값을 조작함.
    현재 알파값을 저장하고, OnTranfomChildrenChange()에서 체크하여 SetParent
    이동에 안전함.

---
+++ Editor/SpriteGroupEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[CustomEditor(typeof(SpriteGroup))]
+public class SpriteGroupEditor : Editor
+{
+    private SpriteGroup myTarget;
+    private void Awake() =>
+        myTarget = target as SpriteGroup;
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        float beforeAlpha = myTarget.Alpha;
+        float afterAlpha = EditorGUILayout.Slider(nameof(myTarget.Alpha), beforeAlpha, 0, 1);
+        if(beforeAlpha != afterAlpha)
+        {
+            myTarget.Alpha = afterAlpha;
+            EditorSceneManager.MarkAllScenesDirty();
+        }
+    }
+}
---
+++ SpriteGroup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SpriteGroup : MonoBehaviour
+{
+    public float Alpha { get { return _Alpha; }
+        set
+        {
+            _Alpha = value;
+            foreach (SpriteRenderer renderer in everyChildSpriteRenderer)
+            {
+                Color col = renderer.color;
+                col.a = _Alpha;
+                renderer.color = col;
+            }
+            
+        }
+    }
+    [HideInInspector] private float _Alpha = 1;
+    private SpriteRenderer[] everyChildSpriteRenderer;
+
+    private void OnEnable()
+    {
+        everyChildSpriteRenderer = GetComponentsInChildren<SpriteRenderer>(true);
+    }
+    private void OnTransformChildrenChanged()
+    {
+        everyChildSpriteRenderer = GetComponentsInChildren<SpriteRenderer>(true);
+        Alpha = Alpha;
+    }
+}
