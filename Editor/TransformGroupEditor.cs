2e731d182368829571d8f750e9cc213bc8c531b7
cupsos <cupsos@gmail.com>
2017-11-10 22:07:47 +0900

    TransformGroup 추가
    복수의 직계 자식 게임오브젝트들을 평균값 기준으로 이동시킴
    복수선택 좌표이동이 좌표를 일치시켜서 해당 코드를 추가함

---
+++ Editor/TransformGroupEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TransformGroup))]
+public class TransformGroupEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        TransformGroup myTarget = target as TransformGroup;
+        Vector3 curAvgPos = myTarget.AveragePosition;
+        Vector3 inputAvgPos = EditorGUILayout.Vector3Field("AvgPos", curAvgPos);
+        if (curAvgPos != inputAvgPos)
+            myTarget.AveragePosition = inputAvgPos;
+        if (GUILayout.Button("avg <- (0,0,0)"))
+            myTarget.AveragePosition = Vector3.zero;
+    }
+}
---
+++ TransformGroup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class TransformGroup : MonoBehaviour
+{
+    public Vector3 AveragePosition
+    {
+        get
+        {
+            Vector3 ret = Vector3.zero;
+            foreach (Transform child in this.transform)
+                ret += child.transform.localPosition;
+            ret /= this.transform.childCount;
+            return ret;
+        }
+        set
+        {
+            Vector3 curAvg = AveragePosition;
+            Vector3 diff = value - curAvg;
+            foreach (Transform child in this.transform)
+                child.transform.localPosition += diff;
+        }
+    }
+}
